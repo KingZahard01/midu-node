6217be259ee11f41da63bf6d88a7a65bf01a20ba
juan garcia <juanjose10312@gmail.com>
2025-06-11 15:51:30 -0700

    Replace semicolons with single quotes for consistent style

--- clase-2/1-http.js
+++ clase-2/1-http.js
@@ -1,28 +1,28 @@
-const http = require("node:http");
+const http = require('node:http')
 
-const desiredPort = process.env.PORT ?? 1234;
+const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
   // establecer el tipo de contenido de la respuesta
-  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
 
-  if (req.url === "/") {
-    res.statusCode = 200; // OK
-    res.end("<h1>Bienvenido a mi página de inicio</h1>");
-  } else if (req.url === "/contacto") {
-    res.statusCode = 200;
-    res.end("<h1>Contacto</h1");
+  if (req.url === '/') {
+    res.statusCode = 200 // OK
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200
+    res.end('<h1>Contacto</h1')
   } else {
-    res.statusCode = 404; // Not Found
-    res.end("<h1>404</h1>");
+    res.statusCode = 404 // Not Found
+    res.end('<h1>404</h1>')
   }
-};
+}
 
-const server = http.createServer(processRequest);
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
-  console.log(`Server listening on port http://localhost:${desiredPort}`);
-});
+  console.log(`Server listening on port http://localhost:${desiredPort}`)
+})
 
 // para reiniciar el servidor automaticamante al hacer cambios en el código, instalar nodemon o usar el comando `node --watch`
 
--- clase-2/2-routing.js
+++ clase-2/2-routing.js
@@ -1,56 +1,56 @@
-const http = require("node:http");
+const http = require('node:http')
 
-const dittoJSON = require("./pokemon/ditto.json");
+const dittoJSON = require('./pokemon/ditto.json')
 
 const processRequest = (req, res) => {
-  const { method, url } = req;
+  const { method, url } = req
 
   switch (method) {
-    case "GET":
+    case 'GET':
       switch (url) {
-        case "/pokemon/ditto":
-          res.setHeader("Content-Type", "application/json; charset=utf-8");
-          return res.end(JSON.stringify(dittoJSON));
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
         default:
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/html; charset=utf-8");
-          return res.end("<h1>404</h1>");
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>404</h1>')
       }
 
-    case "POST":
+    case 'POST':
       switch (url) {
-        case "/pokemon":
-          let body = "";
+        case '/pokemon':
+          let body = ''
 
           // Escuchar el evento 'data'
-          req.on("data", (chunk) => {
-            body += chunk.toString(); // Convertir el Buffer a string
-          });
+          req.on('data', (chunk) => {
+            body += chunk.toString() // Convertir el Buffer a string
+          })
 
           // Escuchar el evento 'end'
-          req.on("end", () => {
-            const data = JSON.parse(body);
+          req.on('end', () => {
+            const data = JSON.parse(body)
             // llamar a una base de datos para guardar la info
             res.writeHead(201, {
-              "Content-Type": "application/json; charset=utf-8",
-            });
+              'Content-Type': 'application/json; charset=utf-8'
+            })
 
-            data.timestamp = Date.now();
-            res.end(JSON.stringify(data));
-          });
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data))
+          })
 
-          break;
+          break
 
         default:
-          res.statusCode = 404;
-          res.setHeader("Content-Type", "text/plain; charset=utf-8");
-          return res.end("404 Not Found");
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+          return res.end('404 Not Found')
       }
   }
-};
+}
 
-const server = http.createServer(processRequest);
+const server = http.createServer(processRequest)
 
 server.listen(1234, () => {
-  console.log("Server listening on port http://localhost:1234");
-});
+  console.log('Server listening on port http://localhost:1234')
+})
