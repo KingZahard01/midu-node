004e7f8c2f727b2e3a083eea3c0a2c5f9f393dee
juan garcia <juanjose10312@gmail.com>
2025-06-17 17:45:10 -0700

    fix: update app.js and movies.js

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,48 +1,122 @@
-const express = require("express");
-const crypto = require("node:crypto");
-const movies = require("./movies.json");
-const validateMovie = require("./schemes/movies.js");
+const express = require('express');
+const cors = require('cors');
+const crypto = require('node:crypto');
+const movies = require('./movies.json');
+const { validateMovie, validatePartialMovie } = require('./schemes/movies.js');
 
 const app = express();
 app.use(express.json());
-app.disable("x-powered-by"); // deshabilita el header x-powered-by: express
+app.use(cors());
+app.disable('x-powered-by'); // deshabilita el header x-powered-by: express
 
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'https://example.com',
+  'https://myapp.com',
+];
+
+// GET
 // Todos los recursos que sean MOVIES se indentifica con /movies
-app.get("/movies", (req, res) => {
+app.get('/movies', (req, res) => {
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin); // Permite el acceso desde el origen especificado
+  }
+
   const { genre } = req.query;
   if (genre) {
-    const filteredMovies = movies.filter((movie) =>
-      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase()),
+    const filteredMovies = movies.filter(movie =>
+      movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()),
     );
     return res.json(filteredMovies);
   }
   res.json(movies);
 });
 
-app.get("/movies/:id", (req, res) => {
+app.get('/movies/:id', (req, res) => {
   const { id } = req.params;
-  const movie = movies.find((movie) => movie.id === id);
+  const movie = movies.find(movie => movie.id === id);
   if (movie) return res.json(movie);
 
-  res.status(404).json({ message: "Movie not found" });
+  res.status(404).json({ message: 'Movie not found' });
 });
 
-app.post("/movies", (req, res) => {
+// POST
+app.post('/movies', (req, res) => {
+  // Validar el cuerpo de la peticion
   const result = validateMovie(req.body);
 
+  // Si la validacion falla, devolver un error
   if (result.error) {
+    // Podria usarse 422 Unprocessable Entity
     return res.status(400).json({ error: JSON.parse(result.error.message) });
   }
 
+  // en base datos, el id es generado por el servidor
   const newMovie = {
     id: crypto.randomUUID(), // crea un uuio v4
-    ...result.data,
+    ...result.data, // no es igual que req.body, porque puede que no tenga todos los campos
   };
 
   // Esto no sería REST porque estamos guardando el estado de la aplicacion en memoria.
   movies.push(newMovie);
 
   res.status(201).json(newMovie);
+});
+
+// PATCH
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body);
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) });
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data, // Actualiza los campos que se envían en el cuerpo de la petición
+  };
+
+  movies[movieIndex] = updateMovie;
+
+  return res.json(updateMovie);
+});
+
+// DELETE
+app.delete('/movies/:id', (req, res) => {
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin); // Permite el acceso desde el origen especificado
+  }
+
+  const { id } = req.params;
+  const movieIndex = movies.findIndex(movie => movie.id === id);
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' });
+  }
+
+  movies.splice(movieIndex, 1); // Elimina la película del array
+  return res.json({ message: 'Movie deleted successfully xd' });
+});
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin');
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', origin); // Permite el acceso desde el origen especificado
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, DELETE, OPTIONS',
+    ); // Permite los métodos especificados
+  }
+
+  res.send(200);
 });
 
 const PORT = process.env.PORT ?? 1234;
--- clase-3/schemes/movies.js
+++ clase-3/schemes/movies.js
@@ -1,33 +1,34 @@
-const z = require("zod");
+const z = require('zod');
 
 const movieSchema = z.object({
   title: z.string({
-    invalid_type_error: "El titulo debe ser un string",
-    required_error: "El titulo es requerido",
+    invalid_type_error: 'El titulo debe ser un string',
+    required_error: 'El titulo es requerido',
   }),
   genre: z.array(
     z.enum([
-      "Action",
-      "Adventure",
-      "Comedy",
-      "Drama",
-      "Fantasy",
-      "Horror",
-      "Thriller",
-      "Sci-Fi",
+      'Action',
+      'Adventure',
+      'Comedy',
+      'Crime',
+      'Drama',
+      'Fantasy',
+      'Horror',
+      'Thriller',
+      'Sci-Fi',
     ]),
     {
-      required_error: "El genero de la pelicula es requerido",
+      required_error: 'El genero de la pelicula es requerido',
       invalid_type_error:
-        "El genero de la pelicula debe ser un array de generos enumerados",
+        'El genero de la pelicula debe ser un array de generos enumerados',
     },
   ),
   year: z.number().int().positive().min(1900).max(2026),
   director: z.string(),
   duration: z.number().int().positive(),
-  rate: number().min(0).max(10),
+  rate: z.number().min(0).max(10).default(5),
   poster: z.string().url({
-    message: "El poster deberia tener una URL válida",
+    message: 'El poster deberia tener una URL válida',
   }),
 });
 
@@ -35,4 +36,8 @@ function validateMovie(object) {
   return movieSchema.safeParse(object);
 }
 
-module.exports = { validateMovie };
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object);
+}
+
+module.exports = { validateMovie, validatePartialMovie };
