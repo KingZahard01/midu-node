3998c23a55ef304d4b073c5ec904ee6c619f1715
juan garcia <juanjose10312@gmail.com>
2025-06-21 16:34:38 -0700

    Finish clase 4

--- clase-4/app.js
+++ clase-4/app.js
@@ -1,20 +1,15 @@
-import express, { json } from 'express'; // require -> commonJS
-import { moviesRouter } from './routes/movies.js';
-import { corsMiddleware } from './middlewares/cors.js';
+import express, { json } from 'express' // require -> commonJS
+import { moviesRouter } from './routes/movies.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
-const app = express();
+const app = express()
+app.use(json())
+app.disable('x-powered-by')
 
-app.use(json());
+app.use('/movies', moviesRouter)
 
-app.use(corsMiddleware);
-
-app.disable('x-powered-by'); // deshabilitar el header X-Powered-By: Express
-
-// Los recursos que sean MOVIES se identifica con /movies
-app.use('/movies', moviesRouter);
-
-const PORT = process.env.PORT ?? 1234;
+const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {
-  console.log(`server listening on port http://localhost:${PORT}`);
-});
+  console.log(`server listening on port http://localhost:${PORT}`)
+})
--- clase-4/models/movie.js
+++ clase-4/models/movie.js
@@ -1,14 +1,65 @@
-import { readJSON } from './utils.js';
+// /models/movie.js
+import { readJSON } from '../utils.js';
+import { randomUUID } from 'node:crypto';
 
 const movies = readJSON('./movies.json');
 
 export class MovieModel {
-  static getAll({ genre }) {
+  // GET ALL
+  static async getAll({ genre }) {
     if (genre) {
       return movies.filter(movie =>
         movie.genre.some(g => g.toLowerCase() === genre.toLowerCase()),
       );
     }
     return movies;
+  }
+
+  // GET by ID
+  static async getById({ id }) {
+    const movie = movies.find(movie => movie.id === id);
+    if (movie) return movie;
+    throw new Error('Movie not found');
+  }
+
+  // POST CREATE
+  static async create({ input }) {
+    const result = validateMovie(input);
+    if (!result.success) {
+      return false;
+    }
+    // en base de datos
+    const newMovie = {
+      id: randomUUID(), // uuid v4
+      ...input,
+    };
+
+    // Esto no sería REST
+    movies.push(newMovie);
+
+    return newMovie;
+  }
+
+  // DELETE
+  static async delete({ id }) {
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    if (movieIndex === -1) return false;
+
+    movies.splice(movieIndex, 1);
+    return true;
+  }
+
+  // PATCH UPDATE
+  static async update({ id, input }) {
+    const movieIndex = movies.findIndex(movie => movie.id === id);
+    if (movieIndex === -1) return false;
+
+    movies[movieIndex] = {
+      ...movies[movieIndex],
+      ...input,
+    };
+    const updatedMovie = movies[movieIndex];
+
+    return updatedMovie;
   }
 }
